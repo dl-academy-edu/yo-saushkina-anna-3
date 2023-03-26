f6310f47d626b93dbcc9b41addee6cf4c57e8604
AnnaSaushkina <saushkinanna@gmail.com>
2023-03-26 21:35:07 +0300

    2 ДЗ - циклы и условия

--- js-homework/2-script.js
+++ js-homework/2-script.js
@@ -1,8 +1,7 @@
 
-// Создать программу, которая запрашивает у пользователя число, в консоль выводит числа от 1 до этого числа, но пропускает числа, которые кратны (делятся без остатка) 4-м.
+// 1. Создать программу, которая запрашивает у пользователя число, в консоль выводит числа от 1 до этого числа, но пропускает числа, которые кратны (делятся без остатка) 4-м.
 
-//1. let userNum = +prompt('Введите число для вывода в консоль');
-
+  let userNum = +prompt('Введите число для вывода в консоль');
   for (let i = 0; i <= userNum; i++) {
     if (i % 4 == 0) continue;
     console.log(i);
@@ -19,8 +18,8 @@ let i = 0;
     factorialResult *= i + 1;
     i++ 
   }
-  console.log(factorialResult);
-  alert (factorialResult);
+    console.log(factorialResult);
+    alert (factorialResult);
 
 
   //3. Написать программу, которая будет получать число и его степень, с помощью цикла for возвести число в степень.
@@ -28,10 +27,10 @@ let i = 0;
   let number = +prompt('Введите число для расчета степени');
   let degree = +prompt('Введите степень');
   let degreeResult = 1;
-  for( i = 0; i < degree; i++ ) {
-    degreeResult *= number;
-  }
-    alert (degreeResult);
+    for( i = 0; i < degree; i++ ) {
+      degreeResult *= number;
+    }
+      alert (degreeResult);
  
 
   //4. Написать проверку, для программ 1-3, чтобы если пользователь вводил неверные данные, например слово вместо числа, то должно вывестись сообщение об ошибке.
@@ -49,7 +48,7 @@ let i = 0;
     let userNumber = +prompt('Угадай число!'); 
     if ( secretNumber !== userNumber ) continue; 
       alert ("Ты угадал!");
-  }
+    }
 
 
  
